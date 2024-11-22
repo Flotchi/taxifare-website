6906801c1adeef0a57ee99b132e61edcb0a844b1
Florian Waeles <florian.waeles@gmail.com>
2024-11-22 15:31:59 +0100

    ok

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import streamlit as st
 import requests
+import pandas as pd
+import datetime
 
 st.markdown('Hello')
 
-pickup_datetime = st.text_input('Date', '')
+date = st.date_input(
+    "select a date",
+    datetime.date(2024, 7, 6))
+time = st.time_input('Selec a time', datetime.time(8, 45))
+pickup_datetime = f'{date} {time}'
 pickup_longitude = st.text_input('pickup_longitude', '')
 pickup_latitude = st.text_input('pickup_latitude', '')
 dropoff_longitude = st.text_input('dropoff_longitude', '')
@@ -17,3 +23,13 @@ params = {'pickup_datetime':pickup_datetime,'pickup_longitude':pickup_longitude,
 response = requests.get(url, params=params).json()
 
 st.write('Fare =', response['fare'])
+
+def get_map_data():
+    return pd.DataFrame(
+        {'lat': [float(pickup_latitude), float(dropoff_latitude)],
+         'lon': [float(pickup_longitude), float(dropoff_longitude)]}
+    )
+
+df = get_map_data()
+
+st.map(df)
